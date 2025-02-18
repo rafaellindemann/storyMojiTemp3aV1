c436c2bdb1afd34570c344e608ca36df09375f04
rafaellindemann <rafaelld@gmail.com>
2025-02-18 16:45:42 -0300

    lançamento mundial da versão pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'StoryMojis',
+        short_name: 'SM:D',
+        description: 'Um jogo de criatividade',
+        theme_color: '#000',
+        icons: [
+          {
+            src: 'moji192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'moji512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
